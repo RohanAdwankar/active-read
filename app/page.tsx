5fb94b81e650ae9606e5dd3f22c862ce1a9c75d2
Rohan Adwankar <39285979+RohanAdwankar@users.noreply.github.com>
2025-03-02 17:25:24 -0800

    removes grey stuff interrupting the dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Reader from '../components/reader';
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-50 py-8">
+    <main className="min-h-screen py-8">
       <Reader />
     </main>
   );
